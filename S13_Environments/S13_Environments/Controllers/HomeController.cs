7503e8cfdb40a8078a337cc653a9652c544cc156
Jeetbareley <Jeetbareley@gmail.com>
2025-06-25 08:46:35 +0530

    Use of Environment Variable in Controller.

--- S13_Environments/S13_Environments/Controllers/HomeController.cs
+++ S13_Environments/S13_Environments/Controllers/HomeController.cs
@@ -4,17 +4,24 @@ namespace S13_Environments.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [Route("/")]
-        [Route("some-route")]
+        //[Route("some-route")]
         public IActionResult Index()
         {
+            ViewBag.EnvironmentName = _webHostEnvironment.EnvironmentName;
             return View();
         }
-        [Route("some-route")]
-        public IActionResult other()
-        {
-            return View();
-        }
+        //[Route("some-route")]
+        //public IActionResult other()
+        //{
+        //    return View();
+        //}
         //[Route("Error")]
         //public IActionResult Error()
         //{
